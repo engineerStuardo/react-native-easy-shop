4741f7f65c8becd13bb1603193eb8a39fd4428da
Italo Stuardo Cortez Silva <ingeniero.stuardo@gmail.com>
2021-06-08 05:07:17 -0600

    adding products json and start building product container with a flatlist

--- src/Screens/Products/ProductContainer.js
+++ src/Screens/Products/ProductContainer.js
@@ -1,10 +1,32 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
+
+import data from '../../../data/products.json';
 
 export const ProductContainer = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(data);
+  }, []);
+
   return (
     <View>
       <Text>Product container</Text>
+      <View style={{ marginTop: 100 }}>
+        <FlatList
+          horizontal
+          data={products}
+          renderItem={({ item }) => <Text>{item.brand}</Text>}
+          keyExtractor={item => item.name}
+        />
+      </View>
     </View>
   );
 };
