6b33118c31313ee962f50036ba51a10c52032337
Italo Stuardo Cortez Silva <ingeniero.stuardo@gmail.com>
2021-06-08 04:53:19 -0600

    building product container

---
+++ src/Screens/Products/ProductContainer.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+
+export const ProductContainer = () => {
+  return (
+    <View>
+      <Text>Product container</Text>
+    </View>
+  );
+};
