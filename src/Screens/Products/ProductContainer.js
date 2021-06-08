bdabc6b6f3808eab0d07cf008e5057291a350576
icortez <icortez@ufg.edu.sv>
2021-06-08 14:01:51 -0600

    building searchbar for products

--- src/Screens/Products/ProductContainer.js
+++ src/Screens/Products/ProductContainer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, ScrollView } from 'react-native';
+import { Searchbar, Icon } from 'react-native-paper';
 import styled from 'styled-components/native';
 
 import { ProductList } from './ProductList';
@@ -10,22 +11,40 @@ const ListContainer = styled.View`
   background-color: gainsboro;
 `;
 
+const InputContainer = styled.View`
+  padding: 10px;
+  background-color: #f7f7f3;
+`;
+
 export const ProductContainer = () => {
   const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     setProducts(data);
   }, []);
 
   return (
-    <ListContainer>
-      <Text>Product Container</Text>
-      <FlatList
-        numColumns={2}
-        data={products}
-        renderItem={item => <ProductList key={item.name} product={item} />}
-        keyExtractor={item => item.name}
-      />
-    </ListContainer>
+    <View>
+      <InputContainer>
+        <Searchbar
+          placeholder='Search'
+          value={search}
+          onSubmitEditing={() => null}
+          onChangeText={text => setSearch(text)}
+        />
+      </InputContainer>
+      <ScrollView>
+        <ListContainer>
+          <Text>Product Container</Text>
+          <FlatList
+            numColumns={2}
+            data={products}
+            renderItem={item => <ProductList key={item.name} product={item} />}
+            keyExtractor={item => item.name}
+          />
+        </ListContainer>
+      </ScrollView>
+    </View>
   );
 };
