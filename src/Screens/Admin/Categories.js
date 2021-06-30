f985de13be4fa1ab2eafa844b1809d066e73e42b
icortez <icortez@ufg.edu.sv>
2021-06-30 16:13:13 -0600

    building add category functionality

--- src/Screens/Admin/Categories.js
+++ src/Screens/Admin/Categories.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { TextInput, IconButton, Colors } from 'react-native-paper';
 import styled from 'styled-components/native';
+import Toast from 'react-native-toast-message';
 
 import { CategoryItem } from './CategoryItem';
 import baseURL from '../../../assets/common/baseUrl';
@@ -24,18 +25,62 @@ const Categories = () => {
   const [categoryName, setCategoryName] = useState();
   const [token, setToken] = useState();
 
-  useEffect(() => {
+  const getToken = () => {
     AsyncStorage.getItem('jwt')
       .then(res => setToken(res))
       .catch(error => console.log(error));
+  };
 
+  const getCategories = () => {
     axios
       .get(`${baseURL}categories`)
       .then(res => {
         setCategories(res.data);
       })
       .catch(error => console.log(error));
+  };
+
+  useEffect(() => {
+    getToken();
+    getCategories();
   }, []);
+
+  const addCategory = () => {
+    if (!categoryName) {
+      Toast.show({
+        topOffset: 60,
+        type: 'error',
+        text1: 'Please enter a category name',
+        text2: 'Please try again',
+      });
+      return;
+    }
+    const config = { headers: { Authorization: `Bearer ${token}` } };
+    const newCategory = { name: categoryName };
+    axios
+      .post(`${baseURL}categories`, newCategory, config)
+      .then(res => {
+        setCategories([...categories, res.data]);
+        if (res.status === 200 || res.status === 201) {
+          Toast.show({
+            topOffset: 60,
+            type: 'success',
+            text1: 'New category added',
+            text2: '',
+          });
+        }
+        setCategoryName('');
+      })
+      .catch(error => {
+        console.log(error);
+        Toast.show({
+          topOffset: 60,
+          type: 'error',
+          text1: 'Something went wrong',
+          text2: 'Please try again',
+        });
+      });
+  };
 
   return (
     <View style={{ flex: 1 }}>
@@ -65,7 +110,7 @@ const Categories = () => {
           icon='plus-circle'
           color={Colors.green500}
           size={40}
-          onPress={() => console.log('Pressed')}
+          onPress={addCategory}
         />
       </BottomContainer>
     </View>
