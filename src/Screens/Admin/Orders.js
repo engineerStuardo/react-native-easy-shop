1e61ab1e40053e4e090728b1c2343fe5211d77c2
icortez <icortez@ufg.edu.sv>
2021-07-01 09:38:47 -0600

    basic structure for showing orders

--- src/Screens/Admin/Orders.js
+++ src/Screens/Admin/Orders.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import axios from 'axios';
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import baseURL from '../../../assets/common/baseUrl';
 
 const Orders = () => {
+  const [orderList, setOrderList] = useState();
+  const [token, setToken] = useState();
+
+  const getToken = () => {
+    AsyncStorage.getItem('jwt')
+      .then(tkn => {
+        setToken(tkn);
+      })
+      .catch(err => console.log(err));
+  };
+
+  const getOrders = () => {
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    axios
+      .get(`${baseURL}orders`, config)
+      .then(res => setOrderList(res.data))
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    getToken();
+  }, []);
+
+  useEffect(() => {
+    getOrders();
+  }, [token]);
+
   return (
     <View>
-      <Text>Orders</Text>
+      <FlatList
+        data={orderList}
+        renderItem={({ item }) => <Text>{item.shippingAddress}</Text>}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
 };
